572eaa16bfd7b86deda6f78aa053e07a2bb34c4e
NicoSB <ncstrbl@gmail.com>
2016-10-11 11:27:53 +0200

    account test

--- src/com/nicosb/uni/bloom_join/SlaveHandler.java
+++ src/com/nicosb/uni/bloom_join/SlaveHandler.java
@@ -1,21 +1,55 @@
 package com.nicosb.uni.bloom_join;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
 
-public class SlaveHandler implements Runnable {
+public class SlaveHandler implements Server {
+	private ArrayList<String> tables;
 
-	private Socket socket;
-	
-	public SlaveHandler(Socket socket) {
+	public SlaveHandler(ArrayList<String> tables) {
 		super();
-		this.socket = socket;
+		this.tables = tables;
 	}
-
 
 	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
+	public void run(String hostName, int port) {	      
+		  // create connection to server
+	      Socket masterSocket;
+	      try{
+		      masterSocket = new Socket(hostName, port);
+		      DataOutputStream output = new DataOutputStream(masterSocket.getOutputStream());
+		      output.writeChar(MasterServer.CHAR_REGISTER);
+		     
+		      // register with covered tables
+		      String tablesString = "";
+		      for(String s: tables){
+		    	  tablesString += s;
+		    	  tablesString += ";";
+		      }
+		      output.writeUTF(tablesString);
+		      DataInputStream input = new DataInputStream(masterSocket.getInputStream());
+		      do{	
+				switch(input.readChar()){
+					case MasterServer.CHAR_BLOOMFILTER:
+						send(output, input.readUTF(), true);
+						break;
+					case MasterServer.CHAR_TUPLES:
+						send(output, input.readUTF(), false);
+						break;
+					case MasterServer.CHAR_TERMINATE:
+						masterSocket.close();
+						break;
+					}		
+		      }while(masterSocket.isConnected());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
+	private void send(DataOutputStream output, String query, boolean bloomed) {
+		
+	}
 }
